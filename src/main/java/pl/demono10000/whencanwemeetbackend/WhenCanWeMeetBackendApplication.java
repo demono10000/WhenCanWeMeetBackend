e06e6e69a9ebee32c99c9e138bb6410b8d6606c1
demono10000 <48636182+demono10000@users.noreply.github.com>
2023-03-23 00:02:01 +0100

    Initial commit

---
+++ src/main/java/pl/demono10000/whencanwemeetbackend/WhenCanWeMeetBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.demono10000.whencanwemeetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhenCanWeMeetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhenCanWeMeetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/demono10000/whencanwemeetbackend/WhenCanWeMeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.demono10000.whencanwemeetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhenCanWeMeetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
